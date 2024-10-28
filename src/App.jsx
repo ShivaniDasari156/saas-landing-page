9155c2d5b47aff7096caece72d7859b855124f8f
aessar <raressilviulazar@gmail.com>
2024-10-28 15:39:32 +0000

    Features section done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./sections/Header";
 import Hero from "./sections/Hero";
+import Features from "./components/Features.jsx";
 
 const App = () => {
   return (
     <main>
       <Header />
       <Hero />
+      <Features />
     </main>
   );
 };

29cf3d4ddfb4e551741efb9ade70f8488fc441a4
aessar <raressilviulazar@gmail.com>
2024-10-29 16:03:10 +0000

    Implemented CountUp in M/A buttons

--- src/sections/Pricing.jsx
+++ src/sections/Pricing.jsx
@@ -1,22 +1,27 @@
 import React, { useState } from "react";
 import { Element } from "react-scroll";
 import clsx from "clsx";
+import { plans } from "../constants/index.jsx";
+import CountUp from "react-countup";git add ./
 
 const Pricing = () => {
   const [monthly, setMonthly] = useState(false);
+
   return (
     <section>
       <Element className="pricing">
         <div className="container">
+          {/* Pricing Header */}
           <div
             className="max-w-960 pricing-head_before relative
-mx-auto border-l border-r border-s2 bg-s1/50 pb-40 pt-28 
+mx-auto border-l border-r border-s2 bg-s1/50 pb-40 pt-28
 max-xl:max-w-4xl max-lg:border-none max-md:pb-32 max-md:pt-16"
           >
             <h3 className="h3 mag-lg:h4 max-md:h5 z-3 relative mx-auto mb-14 max-w-lg text-center text-p4 max-md:mb-11 max-sm:max-w-sm">
               Flexible pricing for teams of all sizes
             </h3>
 
+            {/* Pricing Toggle */}
             <div className="relative z-4 mx-auto flex w-[375px] rounded-3xl border-[3px] border-s4/25 bg-s1/50 p-2 backdrop-blur-[6px] max-md:w-[310px]">
               <button
                 className={clsx("pricing-head_btn", monthly && "text-p4")}
@@ -37,6 +42,8 @@ max-xl:max-w-4xl max-lg:border-none max-md:pb-32 max-md:pt-16"
                 )}
               />
             </div>
+
+            {/* Background Graphics */}
             <div className="pricing-bg">
               <img
                 src="/images/bg-outlines.svg"
@@ -54,9 +61,77 @@ max-xl:max-w-4xl max-lg:border-none max-md:pb-32 max-md:pt-16"
               />
             </div>
           </div>
+
+          {/* Pricing Plans */}
+          <div className="scroll-hide relative z-2 -mt-12 flex items-start max-xl:gap-5 max-xl:overflow-auto max-xl:pt-6">
+            {plans.map((plan, index) => (
+              <div
+                key={plan.id}
+                className="pricing-plan_first pricing-plan_last pricing-plan_odd pricing-plan_even relative border-2 p-7 max-xl:min-w-80 max-lg:rounded-3xl xl:w-[calc(33.33%+2px)]"
+              >
+                {index === 1 && (
+                  <div className="g4 absolute h-330 left-0 right-0 top-0 z-1 rounded-tl-3xl rounded-tr-3xl" />
+                )}
+
+                {/* Plan Logo */}
+                <div
+                  className={clsx(
+                    "absolute left-0 right-0 z-2 flex items-center justify-center",
+                    index === 1 ? "-top-6" : "-top6 xl:-top-11",
+                  )}
+                >
+                  <img
+                    src={plan.logo}
+                    alt={plan.title}
+                    className={clsx(
+                      "object-contain drop-shadow-2xl",
+                      index === 1 ? "size-[120px]" : "size-[88px]",
+                    )}
+                  />
+                </div>
+
+                {/* Plan Details */}
+                <div
+                  className={clsx(
+                    "relative flex flex-col items-center",
+                    index === 1 ? "pt-24" : "pt-12",
+                  )}
+                >
+                  {/* Plan Title */}
+                  <div
+                    className={clsx(
+                      "small-2 rounded-20 relative z-2 mx-auto mb-6 border-2 px-4 py-1.5 uppercase",
+                      index === 1 ? "border-p3 text-p3" : "border-p1 text-p1",
+                    )}
+                  >
+                    {plan.title}
+                  </div>
+
+                  {/* Plan Price */}
+                  <div className="relative z-2 flex items-center justify-center">
+                    <div
+                      className={clsx(
+                        "h-num flex items-start",
+                        index === 1 ? "text-p3" : "text-p4",
+                      )}
+                    >
+                      ${" "}
+                      <CountUp
+                        start={plan.priceMonthly}
+                        end={monthly ? plan.priceMonthly : plan.priceYearly}
+                        duration={0.4}
+                        useEasing={false}
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </Element>
     </section>
   );
 };
+
 export default Pricing;

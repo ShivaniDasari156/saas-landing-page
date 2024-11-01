ce4a1a443d91184afc4658e960d37b205edc1fe7
aessar <raressilviulazar@gmail.com>
2024-11-01 11:48:49 +0000

    Footer section done

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import Features from "./sections/Features.jsx";
 import Pricing from "./sections/Pricing.jsx";
 import Faq from "./sections/Faq.jsx";
 import Testimonials from "./sections/Testimonials.jsx";
+import Download from "./sections/Download.jsx";
+import Footer from "./sections/Footer.jsx";
 
 const App = () => {
   return (
@@ -15,6 +17,8 @@ const App = () => {
       <Pricing />
       <Faq />
       <Testimonials />
+      <Download />
+      <Footer />
     </main>
   );
 };
